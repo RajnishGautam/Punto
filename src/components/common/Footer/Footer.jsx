9b82f58d368fff6530dff746e8f3f2c4b09114b8
Rajnish Kumar <119472379+RajnishGautam@users.noreply.github.com>
2025-07-18 23:25:03 +0530

    APP Removed

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -68,16 +68,16 @@ const Footer = () => {
             <h3 className="contactsection-title">Services</h3>
             <ul className="services-list">
               <li>
-                <a href="/Marketing">Digital Marketing</a>
+                <a href="/digital-marketing">Digital Marketing</a>
               </li>
               <li>
-                <a href="/Branding">UI & UX Designing</a>
+                <a href="/branding">UI & UX Designing</a>
               </li>
               <li>
-                <a href="/Branding">Branding & Identity</a>
+                <a href="/branding">Branding & Identity</a>
               </li>
               <li>
-                <a href="/Development">Mobile App Development</a>
+                <a href="/development">Mobile App Development</a>
               </li>
               {/* <li>
                 <a href="/Development">Web Design & Development</a>
