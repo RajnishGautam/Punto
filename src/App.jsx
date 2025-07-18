f13f5f78c2ccd4a79f54bbf6a46095358b3e41cd
Rajnish Kumar <119472379+RajnishGautam@users.noreply.github.com>
2025-07-18 23:29:46 +0530

    lower case

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Branding from './pages/Branding'
-import DigitalMarketing from './pages/Marketing'
+import Branding from './pages/branding'
+import DigitalMarketing from './pages/marketing'
 import Navbar from './components/common/Navbar/Navbar'
 import Footer from './components/common/Footer/Footer'
-import Development from './pages/Develoment'
+import Development from './pages/develoment'
 import './App.css' // Assuming CSS is here
 
 function App() {
