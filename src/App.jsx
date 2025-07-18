fe0e7ba894c470bbf1ccef0314ea315bcb00c84b
Rajnish Kumar <119472379+RajnishGautam@users.noreply.github.com>
2025-07-18 23:38:39 +0530

    fix: correct imports and file names

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,23 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Branding from './pages/branding'
 import DigitalMarketing from './pages/marketing'
+import Development from './pages/development'
 import Navbar from './components/common/Navbar/Navbar'
 import Footer from './components/common/Footer/Footer'
-import Development from './pages/develoment'
-import './App.css' // Assuming CSS is here
+import './App.css'
 
 function App() {
   return (
-    <div className="app-container">
+    <>
       <Navbar />
-      <div className="content-wrapper">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/branding" element={<Branding />} />
-          <Route path="/digital-marketing" element={<DigitalMarketing/>} />
-          <Route path="/development" element={<Development />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/branding" element={<Branding />} />
+        <Route path="/digital-marketing" element={<DigitalMarketing />} />
+        <Route path="/development" element={<Development />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   )
 }
 
